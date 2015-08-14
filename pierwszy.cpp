57d697903be20fdf1a3bb15db7b44f2751bfc9a4
Lukasz Folwarczyk <l.folwarczyk@gmail.com>
2015-08-14 11:10:53 +0200

    Poprawa2

--- pierwszy.cpp
+++ pierwszy.cpp
@@ -2,10 +2,27 @@
 
 using namespace std;
 
+float sqrt(int);
+
 int main()
 {
+
 	cout<<"Hello World!"<<endl;	
 	cout<<"Jestem w clean_up"<<endl;
+	cout<<"Lol"<<endl;
+	cout<<"Podaj liczbe: ";
+	int i;
+	cin>>i;
+
+	cout<<"liczba podniesiona ^2= "<<sqrt(i)<<endl;
 
 return 0;
 }
+
+
+float sqrt(int i)
+{
+	return i*i;
+}
+
+
